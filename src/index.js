f37985cc9e5d83ea9ed2852985fe30ed66cdbcd3
Samal <samal@example.com>
2026-05-11 09:37:31 +0530

    Feat: Added basic express server setup with dotenv and nodemon

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/ping', (req, res) => {
+  return res.json({ message: "Venture launcher is live!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
